9c4e50afc44491d8e727eaf3d20ffe4defba68ad
Kamal Rahmati <kama1@protonmail.com>
2026-06-28 23:07:59 +0330

    using rest api

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 const list = document.getElementById('list');
-let todos = JSON.parse(window.localStorage.getItem('todos')) || [];
-for (const todo of todos) {
-    list.innerHTML += add(todo);
-}
+getData();
+let todos;
 const addTodoBtn = document.getElementById('addTodoBtn');
 const form = document.getElementById('addtodo');
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const id = formData.get('id');
-    const text = formData.get('text').trim();
-    const done = Boolean(formData.get('done'));
-    if (id) {
-        const todo = todos.find(t => t.id === id);
-        todo.text = text;
-        todo.done = done;
-        save();
-        document.getElementById(id).outerHTML = add(todo);
-    } else {
-        if (text) {
-            const todo = { id: uuidv4(), text, done };
-            todos.push(todo);
-            save();
-            list.innerHTML += add(todo);
+form.addEventListener("submit", async (event) => {
+    try {
+        event.preventDefault();
+        const formData = new FormData(form);
+        const id = formData.get('id');
+        const text = formData.get('text').trim();
+        const done = Boolean(formData.get('done'));
+        if (id) {
+            const todo = todos.find(t => t.id === id);
+            todo.text = text;
+            todo.done = done;
+            await save();
+            document.getElementById(id).outerHTML = add(todo);
+        } else {
+            if (text) {
+                const todo = { id: uuidv4(), text, done };
+                todos.push(todo);
+                await save();
+                list.innerHTML += add(todo);
+            }
         }
+        resetForm();
+        hideForm();
+
+    } catch (error) {
+        console.error('Submit error:', error)
     }
-    resetForm();
-    hideForm();
 });
 
 function add(todo) {
@@ -45,10 +48,10 @@ function resetForm() {
     form.elements.id.value = '';
 }
 
-function check(cb, id) {
+async function check(cb, id) {
     const todo = todos.find(t => t.id === id);
     todo.done = cb.checked;
-    save();
+    await save();
 }
 
 function edit(id) {
@@ -60,16 +63,31 @@ function edit(id) {
     showForm();
 }
 
-function del(id) {
+async function del(id) {
     if (confirm('Are you sure?')) {
         todos = todos.filter(t => t.id != id);
-        save();
+        await save();
         document.getElementById(id).remove();
     }
 }
 
-function save() {
-    window.localStorage.setItem('todos', JSON.stringify(todos));
+async function save() {
+    const url = "http://localhost:3000";
+    try {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(todos)
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+    } catch (error) {
+        console.error(error.message);
+        throw error;
+    }
 }
 
 function showForm() {
@@ -87,4 +105,20 @@ function uuidv4() {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
         (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
     );
+}
+
+async function getData() {
+    const url = "http://localhost:3000";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        todos = await response.json() || [];
+        for (const todo of todos) {
+            list.innerHTML += add(todo);
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
 }
